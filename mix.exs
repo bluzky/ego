f98f7b4e6a02021206e9a28c7c05adc5a40a2d0a
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-10-03 20:13:30 +0700

    disable run

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Ego.MixProject do
         "sass default --no-source-map --style=compressed",
         "phx.digest"
       ],
-      escript: [main_module: Ego.CLI],
+      escript: [main_module: Ego.CLI, app: nil],
       docs: docs(),
       name: "Ego",
       description: description(),
@@ -55,9 +55,6 @@ defmodule Ego.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:solid, github: "bluzky/solid", branch: "custom"}
       {:phoenix, "~> 1.5"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
